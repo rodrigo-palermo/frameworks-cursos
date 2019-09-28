853cbb60ed7a18227e718220004d5618dcc3c540
Rodrigo Palermo <avilapalermo@gmail.com>
2019-09-28 02:39:20 -0300

    Change session files to database (for Heroku).

--- application/config/config.php
+++ application/config/config.php
@@ -215,6 +215,7 @@ $config['allow_get_array'] = TRUE;
 |	2 = Debug Messages
 |	3 = Informational Messages
 |	4 = All Messages
+|	4 = All Messages
 |
 | You can also pass an array with threshold levels to show individual error types
 |
@@ -378,10 +379,10 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'files';
+$config['sess_driver'] = 'database';
 $config['sess_cookie_name'] = 'ci_session';
 $config['sess_expiration'] = 7200;
-$config['sess_save_path'] = NULL;
+$config['sess_save_path'] = 'ci_sessions';
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
 $config['sess_regenerate_destroy'] = FALSE;
--- dbschema/postgres-ci_cursos_pt-br-utf8-20190926.sql
+++ dbschema/postgres-ci_cursos_pt-br-utf8-20190926.sql
@@ -1,4 +1,21 @@
--
+CREATE TABLE "ci_sessions" (
+                               "id" varchar(128) NOT NULL,
+                               "ip_address" varchar(45) NOT NULL,
+                               "timestamp" bigint DEFAULT 0 NOT NULL,
+                               "data" text DEFAULT '' NOT NULL
+);
+
+CREATE INDEX "ci_sessions_timestamp" ON "ci_sessions" ("timestamp");
+-- When sess_match_ip = TRUE
+-- ALTER TABLE ci_sessions ADD PRIMARY KEY (id, ip_address);
+
+-- When sess_match_ip = FALSE
+ALTER TABLE ci_sessions ADD PRIMARY KEY (id);
+
+-- To drop a previously created primary key (use when changing the setting)
+-- ALTER TABLE ci_sessions DROP PRIMARY KEY;
+
+
 -- -----------------------------------------------------
 -- Schema ci_cursos
 -- -----------------------------------------------------
