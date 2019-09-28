fcc28a84cf85d3e566f05a0d3b661c516422a2db
Rodrigo Palermo <avilapalermo@gmail.com>
2019-09-28 19:30:27 -0300

    Set production base_url.

--- application/config/production/config.php
+++ application/config/production/config.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['base_url'] = '';
+$config['base_url'] = 'https://curso-online.herokuapp.com/';
 
 $config['index_page'] = '';
