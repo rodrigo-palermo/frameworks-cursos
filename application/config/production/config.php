0280e8ff11051e53e93819bd3b299a68951cc413
Rodrigo Palermo <avilapalermo@gmail.com>
2019-09-30 18:25:36 -0300

    Fix .gitignore.

---
+++ application/config/production/config.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['base_url'] = 'https://curso-online.herokuapp.com/';
+
+$config['index_page'] = '';
+
+$config['g-recaptha-backend-key'] = '6LemdroUAAAAAGfE-NKT_v3iKivn551Y8Tb-n9TT';
