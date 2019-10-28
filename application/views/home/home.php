5ce52f3d140e0643841b11b5202171605b702ef1
Rodrigo Palermo <avilapalermo@gmail.com>
2019-10-28 18:41:59 -0300

    Adiciona carousel para testes de framerork css.

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -1,17 +1,43 @@
 <!--<img src="--><?//=base_url();?><!--assets/images/main.jpg">-->
 <style>
-	body {
-		background-image: url('<?=base_url();?>assets/images/main.png');
-		background-repeat: no-repeat;
-		/*background-attachment: fixed;*/
-		background-position: center;
-		background-size: cover;
-
-	}
-	section {
-		height: 920px;
-	}
+	/*body {*/
+	/*	background-image: url('*/<?//=base_url();?>/*assets/images/main.png');*/
+	/*	background-repeat: no-repeat;*/
+	/*	/*background-attachment: fixed;*/*/
+	/*	background-position: center;*/
+	/*	background-size: cover;*/
+	/**/
+	/*}*/
+	/*section {*/
+	/*	height: 920px;*/
+	/*}*/
 </style>
 <section>
+	<div id="carouselHomeCourses" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselHomeCourses" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselHomeCourses" data-slide-to="1"></li>
+			<li data-target="#carouselHomeCourses" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="https://cdn.codecademy.com/assets/components/cards/path-card/5b55e668646caa552f8e4d1d.svg" alt="First slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="https://cdn.codecademy.com/assets/components/cards/path-card/5b5001a11d176d7acd8b2bf8.svg" alt="Second slide">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="https://cdn.codecademy.com/assets/components/cards/path-card/5b520caa1d176d21f5a65a61.svg" alt="Third slide">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselHomeCourses" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Anterior</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselHomeCourses" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Próximo</span>
+		</a>
+	</div>
 
 </section>
